7bf1256970da3ad02bdcc580443b8c54abcffdfd
sharkpause <carlmabfredishere@gmail.com>
2025-11-14 00:06:09 +0700

    feat: made DatabaseError for database exceptions past validation stage

--- CustomError.py
+++ CustomError.py
@@ -1,3 +1,7 @@
 class ValidationError(Exception):
     def __init__(self, errors):
         self.errors = errors
+
+class DatabaseError(Exception):
+    def __init__(self, errors):
+        self.errors = errors
