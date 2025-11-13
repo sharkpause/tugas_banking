6b309e56d5668e9190ad422a63a090338ce0fa3a
sharkpause <carlmabfredishere@gmail.com>
2025-11-13 23:41:55 +0700

    feat: changed validation handling to make the server dev's life easier

--- nasabah.py
+++ nasabah.py
@@ -3,6 +3,7 @@ import re
 
 from database import connect_db
 from rekening import Rekening
+from CustomError import ValidationError
 
 db = connect_db()
 
@@ -16,8 +17,13 @@ class Nasabah:
         email = email.strip()
         nomor_telepon = nomor_telepon.strip()
         alamat = alamat.strip()
-        
-        self.validate_parameter(nama, password, email, nomor_telepon, alamat)
+
+        errors = self.validate_parameter(nama, password, email, nomor_telepon, alamat)
+        if(len(errors) == 0):
+            raise ValidationError({
+                'status': 'error',
+                'errors': errors
+            })
 
         self.__nama = nama
         self.__password = password # belum di-hash
@@ -29,21 +35,32 @@ class Nasabah:
 
         self.rekening = Rekening(self.__id)
     
-    def validate_parameter(self, nama: str, password: str, email: str, nomor_telepon: str, alamat: str):
+    def validate_parameter(
+        self,
+        nama: str,
+        password: str,
+        email: str,
+        nomor_telepon: str,
+        alamat: str
+    ):
+        errors = []
+
         if not nama:
-            raise ValueError('Nama tidak bisa kosong')
+            errors.append({'field': 'nama', 'code': 'EMPTY', 'message': 'Nama tidak bisa kosong'})
         if not password:
-            raise ValueError('Password tidak bisa kosong')
+            errors.append({'field': 'password', 'code': 'EMPTY', 'message': 'Password tidak bisa kosong'})
         if not email:
-            raise ValueError('Email tidak bisa kosong')
-        if not re.match(EMAIL_REGEX, email):
-            raise ValueError(f'Format email {email} tidak benar')
+            errors.append({'field': 'email', 'code': 'EMPTY', 'message': 'Email tidak bisa kosong'})
+        elif not re.match(EMAIL_REGEX, email):
+            errors.append({'field': 'email', 'code': 'INVALID_FORMAT', 'message': f'Format email {email} tidak benar'})
         if not nomor_telepon:
-            raise ValueError('Nomor telepon tidak bisa kosong')
-        if not re.match(PHONE_REGEX, nomor_telepon):
-            raise ValueError(f'Format nomor telepon {nomor_telepon} tidak benar')
+            errors.append({'field': 'nomor_telepon', 'code': 'EMPTY', 'message': 'Nomor telepon tidak bisa kosong'})
+        elif not re.match(PHONE_REGEX, nomor_telepon):
+            errors.append({'field': 'nomor_telepon', 'code': 'INVALID_FORMAT', 'message': f'Format nomor telepon {nomor_telepon} tidak benar'})
         if not alamat:
-            raise ValueError(f'Alamat tidak bisa kosong')
+            errors.append({'field': 'alamat', 'code': 'EMPTY', 'message': 'Alamat tidak bisa kosong'})
+
+        return errors
 
     def create_in_database(self):
         query = 'INSERT INTO nasabah (nama, password, email, nomor_telepon, alamat) VALUES (%s, %s, %s, %s, %s)'
@@ -60,15 +77,15 @@ class Nasabah:
             errno = e.errno
 
             if errno == db.DUPLICATE_ERRNO:
-                field = str(e).split("for key '")[1].split("'")[0]
+                field = str(e).split('for key '')[1].split(''')[0]
 
                 raise ValueError(f'Tidak dapat membuat nasabah "{self.__nama}" dikarenakan "{field}" duplikat')
             elif errno == db.NOT_NULL_ERRNO:
-                field = str(e).split("Column '")[1].split("'")[0]
+                field = str(e).split('Column '')[1].split(''')[0]
                 raise ValueError(f'Tidak dapat membuat nasabah "{self.__nama}" dikarenakan "{field}" tidak boleh kosong')
             else:
                 raise RuntimeError(f'Integrity error saat membuat nasabah.\n{e}')
-        except:
+        except Exception as e:
             raise RuntimeError(f'Error saat membuat nasabah.\n{e}')
 
     def telepon(self):
