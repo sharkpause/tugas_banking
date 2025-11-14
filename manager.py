9bc9a3cfbc82c88c329637b687d6cbf6d369552b
sharkpause <carlmabfredishere@gmail.com>
2025-11-14 22:01:02 +0700

    feat: change buat_nasabah_baru to call buat_rekening_baru correctly

--- manager.py
+++ manager.py
@@ -36,7 +36,7 @@ def transaksi(
 def buat_nasabah_baru(nama: str, password: str, email: str, nomor_telepon: str, alamat: str):
     n = Nasabah(nama, password, email, nomor_telepon, alamat)
     n._Nasabah__create_in_database()
-    n.buat_rekening_baru()
+    n._Nasabah__buat_rekening_baru()
 
 # # Testing
 # n = Nasabah('Don', '123', '123@123.com', '081231231111', 'aj')
