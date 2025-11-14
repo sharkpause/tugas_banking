74d6ee55c8eb3a55b18882da0dae2811326cc909
sharkpause <carlmabfredishere@gmail.com>
2025-11-14 20:04:34 +0700

    refactor: added data annotating and changed changes logic to passing a parameter instead

--- rekening.py
+++ rekening.py
@@ -1,5 +1,7 @@
 import random
 
+from typing import Tuple
+
 from database import connect_db
 from CustomClasses import DataChanges
 
@@ -7,13 +9,13 @@ db = connect_db()
 
 class Rekening:
     def __init__(self, id_nasabah: int):
-        self.__nomor_rekening = Rekening.__generate_nomor_rekening()
-        self.__jumlah_saldo = 0
+        self.__nomor_rekening: str = Rekening.__generate_nomor_rekening()
+        self.__jumlah_saldo: int = 0
         
-        self.__id_pemilik = id_nasabah
+        self.__id_pemilik: int = id_nasabah
 
-        query = 'INSERT INTO rekening (id_nasabah, nomor_rekening, jumlah_saldo) VALUES (%s, %s, %s)'
-        values = (self.__id_pemilik, self.__nomor_rekening, self.__jumlah_saldo)
+        query: str = 'INSERT INTO rekening (id_nasabah, nomor_rekening, jumlah_saldo) VALUES (%s, %s, %s)'
+        values: Tuple = (self.__id_pemilik, self.__nomor_rekening, self.__jumlah_saldo)
 
         db.exec_query(query, values)
     
@@ -23,15 +25,13 @@ class Rekening:
 
     def tambah_saldo(self, jumlah_uang: int) -> None:
         self.__jumlah_saldo += jumlah_uang
-        self.__changes[DataChanges.JUMLAH_SALDO] = True
 
-        self.commit()
+        self.commit(DataChanges.JUMLAH_SALDO)
     
     def kurang_saldo(self, jumlah_uang: int) -> None:
         self.__jumlah_saldo -= jumlah_uang
-        self.__changes[DataChanges.JUMLAH_SALDO] = True
 
-        self.commit()
+        self.commit(DataChanges.JUMLAH_SALDO)
     
     @property
     def saldo(self) -> int:
@@ -45,9 +45,9 @@ class Rekening:
     def id_pemilik(self) -> int:
         return self.__id_pemilik
     
-    def commit(self, changes) -> None:
+    def commit(self, changes: DataChanges) -> None:
         if changes == DataChanges.JUMLAH_SALDO:
-            query = 'UPDATE rekening SET jumlah_saldo = %s WHERE nomor_rekening = %s'
-            values = (self.__jumlah_saldo, self.__nomor_rekening)
+            query: str = 'UPDATE rekening SET jumlah_saldo = %s WHERE nomor_rekening = %s'
+            values: Tuple = (self.__jumlah_saldo, self.__nomor_rekening)
 
             db.exec_query(query, values)
