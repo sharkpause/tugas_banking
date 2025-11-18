d92a2a2907c3def3de3bd05d735724dd60ae4912
sharkpause <carlmabfredishere@gmail.com>
2025-11-18 18:40:20 +0700

    feat: added mainloop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
 class MainApp:
     def __init__(self, root):
         self.root = root
         self.root.title = 'Aplikasi'
+
+        self.root.geometry('1000x700')
+
+        self.root.mainloop()
+
+app = MainApp(tk.Tk())
